7c5863125723e1609f915e07aef3a5482b90da7c
Julia <git_user>
2017-11-08 22:15:19 +0300

    contexts files

--- src/main/java/com/htp/lab/spring/course/main/Start.java
+++ src/main/java/com/htp/lab/spring/course/main/Start.java
@@ -7,7 +7,7 @@ import com.htp.lab.spring.course.impls.robot.ModelT1000;
 
 public class Start {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
 		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000Empty");
 		//t1000.fire();
 		System.out.println(t1000.getHand());
